6af648e0996cd1659f6bd0322490e889dbce5198
Gravestorm <gravestormy@gmail.com>
2019-06-27 20:34:25 +0300

    Update

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 Discord bot mainly used in the [Dofus Discord server](https://discord.gg/0RDH0dqUoTRkCjSF).
+
+## Features
+- !chat `message` - Talk with the bot.
+- !d `number channel` - Delete a number of messages, can also transfer the deleted messages to a mentioned channel.
+- !c `hue luminosity` - Change the colour of the Nitro Booster role to a random colour, can also write specific hue and luminosity.
+- Sends a message to #almanax channels with the offering for the current day, refreshes every day.
+- Gives the Streaming role to users who are streaming and removes it when they finish, can also limit the giving of the role to users who also have the Streamer role.
+- Changes the colour of the Nitro Booster role to a random colour every hour.
+
+## Permissions Needed
+- Read Text Channels
+- Read Message History
+- Send Messages
+- Manage Messages
+- Manage Roles (The bot's role also needs to be above the Streaming/Streamer/Nitro Booster roles)
--- index.js
+++ index.js
@@ -3,30 +3,60 @@ import cheerio from 'cheerio';
 import Cleverbot from 'cleverbot';
 import Discord from 'discord.js';
 import nconf from 'nconf';
+import randomColor from 'randomcolor';
 import rp from 'request-promise';
 import startExpress from './express';
 const client = new Discord.Client();
+const embed = new Discord.RichEmbed();
 let clever = new Cleverbot({
   key: nconf.get('CLEVERBOT')
 });
 const commands = {
   chat: {
-    process: function(client, msg, suffix) {
-      if (!nconf.get('CLEVERBOT')) return;
-      if (!suffix) suffix = 'Hello.';
-      msg.channel.startTyping();
-      clever.query(suffix.replace(/([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '')).then((response) => {
-        msg.channel.send(response.output);
-        msg.channel.stopTyping();
-      });
+    process: (client, msg, suffix) => {
+      if (nconf.get('CLEVERBOT')) {
+        if (!suffix) suffix = 'Hello.';
+        msg.channel.startTyping();
+        clever.query(suffix.replace(/([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, 'emoji')).then((response) => {
+          msg.channel.send(response.output);
+          msg.channel.stopTyping();
+        });
+      }
     }
   },
   d: {
-    process: function(client, msg, suffix) {
+    process: (client, msg, suffix, suffix1) => {
       msg.member.guild.fetchMembers().then(g => {
         if (g.me.permissions.has('MANAGE_MESSAGES') && msg.member.permissions.has('MANAGE_MESSAGES')) {
-          if (suffix > 100) suffix = 100;
-          msg.channel.bulkDelete(suffix);
+          if (suffix1 > 100) suffix1 = 100;
+          msg.delete();
+          setTimeout(() => {
+            if (msg.mentions.channels.first()) {
+              msg.channel.bulkDelete(suffix1).then(msgs => {
+                msgs.sort((b, a) => b.id - a.id).forEach(m => {
+                  embed.setAuthor(m.author.username, m.author.avatarURL).setDescription(m.content).setTimestamp(m.createdTimestamp).setColor(randomColor());
+                  msg.mentions.channels.first().send(embed);
+                });
+              });
+            } else {
+              msg.channel.bulkDelete(suffix1);
+            }
+          }, 1000);
+        }
+      });
+    }
+  },
+  c: {
+    process: (client, msg, suffix, suffix1, suffix2) => {
+      msg.member.guild.fetchMembers().then(g => {
+        if (g.me.permissions.has('MANAGE_MESSAGES') && g.me.permissions.has('MANAGE_ROLES') && msg.member.permissions.has('VIEW_AUDIT_LOG')) {
+          if (!suffix1) suffix1 = 'random';
+          if (!suffix2) suffix2 = 'random';
+          msg.delete();
+          g.roles.find(r => r.name === '✨Nitro Booster✨').setColor(randomColor({
+            hue: suffix1,
+            luminosity: suffix2
+          }));
         }
       });
     }
@@ -125,27 +155,27 @@ client.on('ready', () => {
     setInterval(() => {
       client.guilds.forEach(g => {
         g.fetchMembers().then(g => {
-          if (g.me.permissions.has('MANAGE_ROLES') && g.roles.find(r => r.name === 'Streamer') && g.roles.find(r => r.name === 'Streaming')) {
+          if (g.me.permissions.has('MANAGE_ROLES') && g.roles.find(r => r.name === '🎥Streamer🎥') && g.roles.find(r => r.name === '🎥Streaming🎥')) {
             g.members.forEach((member) => {
-              if (member.roles.has(g.roles.find(r => r.name === 'Streamer').id) === true && member.roles.has(g.roles.find(r => r.name === 'Streaming').id) !== true && member.user.presence.game && member.user.presence.game.streaming === true) {
-                member.addRole(g.roles.find(r => r.name === 'Streaming').id);
-              } else if (member.roles.has(g.roles.find(r => r.name === 'Streaming').id) === true) {
+              if (member.roles.has(g.roles.find(r => r.name === '🎥Streamer🎥').id) === true && member.roles.has(g.roles.find(r => r.name === '🎥Streaming🎥').id) !== true && member.user.presence.game && member.user.presence.game.streaming === true) {
+                member.addRole(g.roles.find(r => r.name === '🎥Streaming🎥').id);
+              } else if (member.roles.has(g.roles.find(r => r.name === '🎥Streaming🎥').id) === true) {
                 if (!member.user.presence.game) {
-                  member.removeRole(g.roles.find(r => r.name === 'Streaming').id);
+                  member.removeRole(g.roles.find(r => r.name === '🎥Streaming🎥').id);
                 } else if (member.user.presence.game && member.user.presence.game.streaming !== true) {
-                  member.removeRole(g.roles.find(r => r.name === 'Streaming').id);
+                  member.removeRole(g.roles.find(r => r.name === '🎥Streaming🎥').id);
                 }
               }
             });
-          } else if (g.me.permissions.has('MANAGE_ROLES') && g.roles.find(r => r.name === 'Streaming')) {
+          } else if (g.me.permissions.has('MANAGE_ROLES') && g.roles.find(r => r.name === '🎥Streaming🎥')) {
             g.members.forEach((member) => {
-              if (member.roles.has(g.roles.find(r => r.name === 'Streaming').id) !== true && member.user.presence.game && member.user.presence.game.streaming === true) {
-                member.addRole(g.roles.find(r => r.name === 'Streaming').id);
-              } else if (member.roles.has(g.roles.find(r => r.name === 'Streaming').id) === true) {
+              if (member.roles.has(g.roles.find(r => r.name === '🎥Streaming🎥').id) !== true && member.user.presence.game && member.user.presence.game.streaming === true) {
+                member.addRole(g.roles.find(r => r.name === '🎥Streaming🎥').id);
+              } else if (member.roles.has(g.roles.find(r => r.name === '🎥Streaming🎥').id) === true) {
                 if (!member.user.presence.game) {
-                  member.removeRole(g.roles.find(r => r.name === 'Streaming').id);
+                  member.removeRole(g.roles.find(r => r.name === '🎥Streaming🎥').id);
                 } else if (member.user.presence.game && member.user.presence.game.streaming !== true) {
-                  member.removeRole(g.roles.find(r => r.name === 'Streaming').id);
+                  member.removeRole(g.roles.find(r => r.name === '🎥Streaming🎥').id);
                 }
               }
             });
@@ -154,13 +184,25 @@ client.on('ready', () => {
       });
     }, 60000); // 60000 = 1 minute
   }, 6000);
+  setTimeout(() => {
+    setInterval(() => {
+      client.guilds.forEach(g => {
+        if (g.me.permissions.has('MANAGE_ROLES') && g.roles.find(r => r.name === '✨Nitro Booster✨')) {
+          g.roles.find(r => r.name === '✨Nitro Booster✨').setColor(randomColor());
+        }
+      });
+    }, 3600000); // 3600000 = 60 minutes
+  }, 9000);
 });
 client.on('message', msg => {
   if (msg.content[0] === '!') {
     const command = msg.content.toLowerCase().split(' ')[0].substring(1);
     const suffix = msg.content.substring(command.length + 2);
+    const suffix1 = msg.content.toLowerCase().split(' ')[1];
+    const suffix2 = msg.content.toLowerCase().split(' ')[2];
+    const suffix3 = msg.content.toLowerCase().split(' ')[3];
     const cmd = commands[command];
-    if (cmd) return cmd.process(client, msg, suffix);
+    if (cmd) return cmd.process(client, msg, suffix, suffix1, suffix2, suffix3);
     return;
   }
   if (msg.channel.name === 'announcements' || msg.channel.name === 'annonces') {
