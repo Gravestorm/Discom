2d39a4a415948f1c02d8995090d54136722a9df2
Gravestorm <gravestormy@gmail.com>
2020-03-31 01:01:47 +0300

    Nickname cleanup tweaks

--- plugins/nickname.js
+++ plugins/nickname.js
@@ -1,14 +1,18 @@
 import nconf from 'nconf';
-const pre = ['!', '"', '\'', '( ', '(aa'];
+const pre = ['!', '"', '\'', ,'#', '$', '%', '&', '((', '(! ', '(aa'];
 
 module.exports = (client) => {
-  if (nconf.get('NICKNAME') != 'true') return;
+  if (!nconf.get('ROLE_NICKNAME')) return;
   setInterval(() => {
     client.guilds.forEach(g => {
-      if (g.me.permissions.has('MANAGE_NICKNAMES')) {
+      if (g.me.permissions.has('MANAGE_NICKNAMES') && g.me.permissions.has('MANAGE_ROLES') && g.roles.find(r => r.name === nconf.get('ROLE_NICKNAME'))) {
         g.members.forEach(m => {
           pre.forEach(e => {
-            if (m.displayName.toLowerCase().startsWith(e)) m.setNickname(`Care-Bear${Math.floor(Math.random() * 1000).toString().padStart(3, '0')}`);
+            if (m.displayName.startsWith('( ')) m.setNickname(m.displayName.replace(/ /gi, ''));
+            if (m.displayName.toLowerCase().startsWith(e)) {
+              m.setNickname(`Care-Bear${Math.floor(Math.random() * 1000).toString().padStart(3, '0')}`);
+              m.addRole(g.roles.find(r => r.name === nconf.get('ROLE_NICKNAME')).id);
+            }
           });
         });
       }
