9ca66e41459024ede20e941f6d4923bcb2d18168
Gravestorm <gravestormy@gmail.com>
2022-07-27 16:35:41 +0300

    Update image.js

--- commands/image.js
+++ commands/image.js
@@ -5,14 +5,14 @@ const delay = ms => new promise(resolve => setTimeout(resolve, ms))
 
 module.exports = {
   data: new SlashCommandBuilder().setName('image').setDescription('Grant the permission to send images for a user')
-  .addUserOption(option => option.setName('user').setDescription('Select a user').setRequired(true))
-  .addNumberOption(option => option.setName('num').setDescription('Enter the amount of time in minutes')),
+    .addUserOption(option => option.setName('user').setDescription('Select a user').setRequired(true))
+    .addNumberOption(option => option.setName('minutes').setDescription('Enter the amount of time in minutes')),
   async execute(interaction) {
     if (!nconf.get('ROLE_IMAGE')) return
     await interaction.deferReply({ ephemeral: true })
     await interaction.options.getMember('user').roles.add(nconf.get('ROLE_IMAGE'))
     await interaction.editReply({ content: 'Permission given successfully.', ephemeral: true })
-    interaction.options.getNumber('num') ? await delay(interaction.options.getNumber('num')) : await delay(120000)
+    interaction.options.getNumber('minutes') ? await delay(interaction.options.getNumber('minutes') * 60000) : await delay(120000)
     await interaction.options.getMember('user').roles.remove(nconf.get('ROLE_IMAGE'))
     await interaction.editReply({ content: 'Permission removed successfully.', ephemeral: true })
   }
