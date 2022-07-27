ace59ec35fccb6969a72ee06e117b6ae1d611bbe
Gravestorm <gravestormy@gmail.com>
2022-07-27 16:27:51 +0300

    Almanax update
    Addition of the /almanax command
    Improved the almanax plugin
    Added the ability to specify minutes in the /image command

---
+++ commands/almanax.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { MessageEmbed } = require('discord.js')
+const cheerio = require('cheerio')
+const fetch = require('request-promise')
+const fs = require('node:fs')
+const random = require('randomcolor')
+const almanax = JSON.parse(fs.readFileSync('almanax.json'))
+
+module.exports = {
+  data: new SlashCommandBuilder().setName('almanax').setDescription('Fetches the Almanax of a specific day')
+    .addStringOption(option => option.setName('date').setDescription('Enter the day in the following format: yyyy-mm-dd (e.g. 2022-12-31)').setRequired(true)),
+  async execute(interaction) {
+    let date
+    let datef = '2012-09-19'.split('-')
+    let datet = '9999-12-31'.split('-')
+    let datec = interaction.options.getString('date').toLocaleString('LT', { year: 'numeric', month: '2-digit', day: '2-digit' }).split('-')
+    let df = new Date(datef[0], parseInt(datef[1]) - 1, datef[2])
+    let dt = new Date(datet[0], parseInt(datet[1]) - 1, datet[2])
+    let dc = new Date(datec[0], parseInt(datec[1]) - 1, datec[2])
+    dc > df && dc < dt ? date = new Date(interaction.options.getString('date')).toLocaleString('LT', { year: 'numeric', month: '2-digit', day: '2-digit' }) : date = new Date().toLocaleString('LT', { year: 'numeric', month: '2-digit', day: '2-digit' })
+    await fetch({ url: `https://www.krosmoz.com/en/almanax/${date}?game=dofus`, encoding: 'utf8', transform: (body) => { return cheerio.load(body) } }).then(async ($) => {
+      let d = almanax.find(d => $('#achievement_dofus .mid .more .more-infos p').first().text().trim().replace('Quest: Offering for ', '') === d.N)
+      await interaction.reply({ content: dc > df && dc < dt ? ' ' : 'Correct date format: yyyy-mm-dd (e.g. 2022-12-31)', embeds: [new MessageEmbed().setTitle(`**${date} | ${d.N} | ${d.R}**`).setDescription(`**Objet:** ${d.IFR}\n${d.BFR}\n\n\n**Item:** ${d.IEN}\n${d.BEN}`).setThumbnail(d.I).setColor(random())], ephemeral: true })
+    }).catch(err => { throw err })
+  }
+}
--- plugins/almanax.js
+++ plugins/almanax.js
@@ -1,30 +1,22 @@
+const { MessageEmbed } = require('discord.js')
 const cheerio = require('cheerio')
 const cron = require('cron').CronJob
 const fetch = require('request-promise')
+const fs = require('node:fs')
 const nconf = require('nconf')
+const random = require('randomcolor')
+const almanax = JSON.parse(fs.readFileSync('almanax.json'))
 
-module.exports = (client) => {
+module.exports = async (client) => {
+
   if (!nconf.get('CHANNEL_ALMANAX')) return
   new cron({
-    cronTime: '00 00 00 * * *',
+    cronTime: '00 00 10 * * *',
     onTick: () => {
       client.channels.fetch(nconf.get('CHANNEL_ALMANAX')).then(c => c.messages.fetch({ limit: 1, cache: false }).then(m => m.last().delete()))
-      let text = ''
-      fetch({ url: 'https://www.krosmoz.com/en/almanax?game=dofus', encoding: 'utf8', transform: (body) => { return cheerio.load(body) } }).then(($) => {
-        let questen = $('#achievement_dofus .mid .more .more-infos p').first().text().trim()
-        let offeren = $('#achievement_dofus .mid .more .more-infos .more-infos-content .fleft').first().text().trim()
-        let bonusen = $('#achievement_dofus .mid .more').first().children().remove('div.more-infos').end().text().trim()
-        let mainen = $('#achievement_dofus .mid').first().children().remove('div.more').end().text().trim()
-        text += `__**${new Date().toLocaleString('LT', { timeZone: 'Europe/Paris', year: 'numeric', month: '2-digit', day: '2-digit' })}**__\n\n**${mainen}**\n${bonusen}\n\n**${questen}**\n${offeren}`
-        fetch({ url: 'https://www.krosmoz.com/fr/almanax?game=dofus', encoding: 'utf8', transform: (body) => { return cheerio.load(body) } }).then(($) => {
-          let questfr = $('#achievement_dofus .mid .more .more-infos p').first().text().trim()
-          let offerfr = $('#achievement_dofus .mid .more .more-infos .more-infos-content .fleft').first().text().trim()
-          let image = $('#achievement_dofus .mid .more .more-infos .more-infos-content img').attr('src')
-          let bonusfr = $('#achievement_dofus .mid .more').first().children().remove('div.more-infos').end().text().trim()
-          let mainfr = $('#achievement_dofus .mid').first().children().remove('div.more').end().text().trim()
-          text += `\n\n\n**${mainfr}**\n${bonusfr}\n\n**${questfr}**\n${offerfr}\n\n${image.replace('.w75h75', '')}`
-          client.channels.fetch(nconf.get('CHANNEL_ALMANAX')).then(c => c.send(text).then(m => m.crosspost()))
-        }).catch(err => { throw err })
+      fetch({ url: `https://www.krosmoz.com/en/almanax?game=dofus`, encoding: 'utf8', transform: (body) => { return cheerio.load(body) } }).then(($) => {
+        let d = almanax.find(d => $('#achievement_dofus .mid .more .more-infos p').first().text().trim().replace('Quest: Offering for ', '') === d.N)
+        client.channels.fetch(nconf.get('CHANNEL_ALMANAX')).then(c => c.send({ embeds: [new MessageEmbed().setTitle(`**${new Date().toLocaleString('LT', { timeZone: 'Europe/Paris', year: 'numeric', month: '2-digit', day: '2-digit' })} | ${d.N} | ${d.R}**`).setDescription(`**Objet:** ${d.IFR}\n${d.BFR}\n\n\n**Item:** ${d.IEN}\n${d.BEN}`).setImage(d.I).setColor(random())] }).then(m => m.crosspost()))
       }).catch(err => { throw err })
     },
     start: true,
