23b680c9dfb06331e5f92fdcb11f0db20db727f2
Gravestorm <gravestormy@gmail.com>
2022-08-03 12:54:44 +0300

    Fix streaming plugin

--- plugins/streaming.js
+++ plugins/streaming.js
@@ -6,7 +6,7 @@ module.exports = (client) => {
   client.guilds.fetch(nconf.get('SERVER')).then(g => g.members.fetch().then(() => {
     g.roles.cache.get(nconf.get('ROLE_STREAMER')).members.map(m => {
       let stream
-      m.presence?.activities.forEach(a => { if (a.type.toLowerCase() === 'streaming') stream = true })
+      m.presence?.activities.forEach(a => { if (a.type === 1) stream = true })
       if (m.roles.resolve(nconf.get('ROLE_STREAMING')) === null && stream === true) m.roles.add(g.roles.resolve(nconf.get('ROLE_STREAMING')))
       if (m.roles.resolve(nconf.get('ROLE_STREAMING')) !== null && stream !== true) m.roles.remove(g.roles.resolve(nconf.get('ROLE_STREAMING')))
     })
