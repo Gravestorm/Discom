93407dd7c477ea451790b6ba24aec57a25d97edd
Gravestorm <gravestormy@gmail.com>
2023-02-24 16:43:08 +0200

    Almanax plugin fix

--- plugins/almanax.js
+++ plugins/almanax.js
@@ -10,8 +10,8 @@ module.exports = async (client) => {
   if (!nconf.get('CHANNEL_ALMANAX')) return
   setInterval(() => {
     client.channels.fetch(nconf.get('CHANNEL_ALMANAX')).then(c => c.messages.fetch({ limit: 1, cache: false }).then(m => {
-      if (m.last()?.embeds[0].data.title.substring(2, 12) === new Date().toLocaleString('LT', { timeZone: 'Europe/Paris', year: 'numeric', month: '2-digit', day: '2-digit' })) return
-      m.last().delete()
+      if (m.last()?.embeds[0]?.data.title.substring(2, 12) === new Date().toLocaleString('LT', { timeZone: 'Europe/Paris', year: 'numeric', month: '2-digit', day: '2-digit' })) return
+      m.last()?.delete()
       fetch({ url: `https://www.krosmoz.com/en/almanax?game=dofus`, encoding: 'utf8', transform: (body) => { return cheerio.load(body) } }).then(($) => {
         let d = almanax.find(d => $('#achievement_dofus .mid .more .more-infos p').first().text().trim().replace('Quest: Offering for ', '') === d.N)
         client.channels.fetch(nconf.get('CHANNEL_ALMANAX')).then(c => c.send({ embeds: [new EmbedBuilder().setTitle(`**${new Date().toLocaleString('LT', { timeZone: 'Europe/Paris', year: 'numeric', month: '2-digit', day: '2-digit' })} | ${d.N} | ${d.R}**`).setDescription(`**Objet:** ${d.IFR}\n${d.BFR}\n\n\n**Item:** ${d.IEN}\n${d.BEN}`).setImage(d.I).setColor(random())] }).then(m => m.crosspost()))
