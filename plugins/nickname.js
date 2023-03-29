3f8c1651c21c4b5d8a4a30387984153654d3b381
Gravestorm <gravestormy@gmail.com>
2023-03-29 19:53:32 +0300

    Fixed nickname plugin crash and updated almanax list

--- plugins/nickname.js
+++ plugins/nickname.js
@@ -5,8 +5,8 @@ module.exports = (client) => {
   if (!nconf.get('NICKNAME') || !nconf.get('SERVER')) return
   setInterval(() => {
     client.guilds.fetch(nconf.get('SERVER')).then(g => g.members.fetch().then(m => m.forEach(m => {
-      if (m.displayName.startsWith('( ') || m.displayName.toLowerCase().startsWith('(agr) ')) return m.setNickname(m.displayName.replace(/ /gi, ''))
-      if (m.displayName.startsWith('(') && m.displayName.charCodeAt(1) < 65) return m.setNickname(m.displayName.substring(e.length))
+      if (m.displayName.startsWith('( ')) return m.setNickname(m.displayName.replace(/ /gi, ''))
+      if (m.displayName.startsWith('(') && m.displayName.charCodeAt(1) < 65) return m.setNickname(m.displayName.substring(m.length))
       pre.forEach(e => { if (m.displayName.toLowerCase().startsWith(e)) { return m.setNickname(m.displayName.substring(e.length)) } })
     })))
   }, 1800000) // 1800000 = 30 minutes
