c2087ed15fc802218d4a127131ac2c431c6b3f6b
Gravestorm <gravestormy@gmail.com>
2024-08-19 01:43:15 +0300

    Update colour to change colours of personalized roles

--- commands/colour.js
+++ commands/colour.js
@@ -1,15 +1,53 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
 const nconf = require('nconf')
 const random = require('randomcolor')
-const requiredKeys = ['ROLE_NITRO']
+const requiredKeys = ['ROLE_NITRO', 'ROLE_HELPER', 'ROLE_OMEGA']
+
+function encode(id) {
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let num = BigInt(id)
+  let result = ''
+  while (num > 0) {
+    result = chars[num % 62n] + result
+    num = num / 62n
+  }
+  return result || '0'
+}
+
+function isHex(colour) {
+  return /^#?[0-9A-F]{6}$/i.test(colour)
+}
 
 module.exports = {
-  data: new SlashCommandBuilder().setName('colour').setDescription('Change the colour of the Nitro Booster role to a random or specific colour')
-    .addStringOption(option => option.setName('hue').setDescription('Write a colour name or hex code'))
-    .addStringOption(option => option.setName('luminosity').setDescription('Choose the brightness of the colour').addChoices({ name: 'Bright', value: 'bright' }).addChoices({ name: 'Light', value: 'light' }).addChoices({ name: 'Dark', value: 'dark' })),
+  data: new SlashCommandBuilder().setName('colour').setDescription('Change the colour of your name to a random or specific colour')
+    .addStringOption(option => option.setName('colour').setDescription('Write a colour name, number, hex code or rgb array')),
   async execute(interaction) {
     if (!requiredKeys.every(key => nconf.get(key))) return
-    await interaction.guild.roles.resolve(nconf.get('ROLE_NITRO')).setColor(random({ luminosity: interaction.options.getString('luminosity') ? interaction.options.getString('luminosity') : 'random', hue: interaction.options.getString('hue') ? interaction.options.getString('hue') : 'random' }))
+    const guild = interaction.guild
+    const member = interaction.member
+    const roleName = encode(member.user.id)
+    let role = guild.roles.cache.find(r => r.name === roleName)
+    let colourInput = interaction.options.getString('colour') || random()
+    if (!isHex(colourInput)) {
+      try {
+        colourInput = random({ hue: colourInput })
+      } catch (err) {
+        colourInput = random()
+      }
+    }
+    if (isHex(colourInput) && !colourInput.startsWith('#')) colourInput = `#${colourInput}`
+    if (!role) {
+      role = await guild.roles.create({
+        name: roleName,
+        color: colourInput,
+        position: guild.roles.cache.size - 5,
+        mentionable: false,
+        hoist: false
+      })
+      await member.roles.add(role)
+    } else {
+      await role.setColor(colourInput)
+    }
     await interaction.reply({ content: 'Colour changed successfully.\nLa couleur a changé avec succès.', ephemeral: true })
   }
 }
